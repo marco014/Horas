31153a14586f192b33874ce9ab1d0a346d04632b
DIAZ-1 <marcodarineldiaz@gmail.com>
2022-06-30 17:07:53 -0500

    Actualizacion 1

--- src/hora/models/Procesos.java
+++ src/hora/models/Procesos.java
@@ -5,7 +5,7 @@ public class Procesos {
     private int min;
     private int seg;
     public boolean validarDatos(int hora, int min, int seg){
-        if (hora >= 0  && hora <= 24 && min >= 0 && min < 60 && seg >= 0 && seg < 60){
+        if (hora >= 0  && hora < 24 && min >= 0 && min < 60 && seg >= 0 && seg < 60){
             return true;
         }else {
             return false;
